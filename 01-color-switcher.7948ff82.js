704a0a62d573dee89269ba30e6d4bf04c98be796
ElenaSs95 <104262592+ElenaSs95@users.noreply.github.com>
2022-09-29 11:44:12 +0000

    Deploying to gh-pages from @ ElenaSs95/goit-js-hw-09@94998d96ec8e74417352de2a2ec7f910ab278407 🚀

---
+++ 01-color-switcher.7948ff82.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body"),n=(document.querySelector(".color"),null),c=function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))};t.addEventListener("click",(function(){n=setInterval(c,1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.7948ff82.js.map
